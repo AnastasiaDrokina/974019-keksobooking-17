4671d7572ad6f118efdf20237090a3d8f9ecc3b6
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2019-07-04 16:02:57 +0200

    Fix array name

--- js/filter.js
+++ js/filter.js
@@ -2,11 +2,11 @@
 (function () {
   var typeInput = document.querySelector('#housing-type');
   typeInput.addEventListener('change', function () {
-    var typeFilter = window.adverts.filter(function (advert) {
+    var typeFilters = window.adverts.filter(function (advert) {
       return advert.offer.type === typeInput.value;
     });
 
-    var typeLimit = typeFilter.slice(0, 5);
+    var typeLimit = typeFilters.slice(0, 5);
     var pins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
     pins.forEach(function (pin) {
       pin.parentNode.removeChild(pin);
